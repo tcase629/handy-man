945ce69888d73cde8ab6540cb12ec8385fc3ac0a
Tyler Case <tcase629@yahoo.com>
2021-09-24 07:41:18 -0600

    fixed models

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,3 +1,5 @@
 class Service < ApplicationRecord
   belongs_to :worker
+  has_many :comments, dependent: :destroy
+  validates :serv_type, :cost, presence: true
 end
--- app/models/worker.rb
+++ app/models/worker.rb
@@ -1,2 +1,4 @@
 class Worker < ApplicationRecord
+  has_many :services, dependent: :destroy
+  validates :name, :specialty, :phone, presence: true
 end
