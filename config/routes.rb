5053c45cddac31610c17dabb01fcb5700463f996
Tyler Case <tcase629@yahoo.com>
2021-09-25 18:42:21 -0600

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   namespace :api do
 
     resources :workers do
-      resources :services do
-        resources :comments
-      end
+      resources :services 
+    end
+    
+    resources :services do
+      resources :comments
     end
   end
 end
